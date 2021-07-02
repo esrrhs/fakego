a025ebd5853f894d0cd9bd4b770e6e915f05d837
esrrhs <esrrhs@163.com>
2021-07-02 17:13:47 +0800

    add

--- bindfunc_test.go
+++ bindfunc_test.go
@@ -16,9 +16,23 @@ func Test0003(t *testing.T) {
 	RegFunc("testReg", testReg)
 }
 
+type TestStruct struct {
+	a int
+}
+
+func (t *TestStruct) testReg(a int) int {
+	return a - t.a
+}
+
 func Test0004(t *testing.T) {
-	gfs.cfg.OpenLog = true
 	vo := reflect.ValueOf(testReg)
 	ret := vo.Call([]reflect.Value{reflect.ValueOf(2), reflect.ValueOf("2")})
-	fmt.Printf("Invoke %v\n", ret)
+	fmt.Printf("Invoke %v\n", ret[0].Int())
+}
+
+func Test0005(t *testing.T) {
+	ts := &TestStruct{10}
+	vo := reflect.ValueOf(ts.testReg)
+	ret := vo.Call([]reflect.Value{reflect.ValueOf(2)})
+	fmt.Printf("Invoke %v\n", ret[0].Int())
 }
